ff49c728ee11d83d6b365ffb1233a436acca4980
SaltyRex <72558652+SaltyRex@users.noreply.github.com>
2020-10-27 20:38:48 +0000

    Add files via upload

---
+++ core/assets/map/pictureWaterTiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="pictureWaterTiles" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="pictureWaterTiles.png" width="259" height="194"/>
+ <tile id="25">
+  <properties>
+   <property name="WaterFrame" value="2"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="WaterFrame" value="3"/>
+  </properties>
+ </tile>
+</tileset>
