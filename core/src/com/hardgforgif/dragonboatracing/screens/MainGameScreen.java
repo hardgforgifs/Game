ef887f7d367f6236cdd7648c86878eb48526755d
rmil <rhys.milling@googlemail.com>
2020-11-08 23:34:33 +0000

    Experimental main menu

--- core/src/com/hardgforgif/dragonboatracing/screens/MainGameScreen.java
+++ core/src/com/hardgforgif/dragonboatracing/screens/MainGameScreen.java
@@ -1,4 +1,11 @@
 package com.hardgforgif.dragonboatracing.screens;
 
 public class MainGameScreen {
+    public static final float SPEED = 300;
+
+    public static final float BOAT_ANIMATION_SPEED = 0.5f;
+    public static final int BOAT_WIDTH_PIXEL = 17;
+    public static final int BOAT_HEIGHT_PIXEL = 32;
+    public static final int BOAT_WIDTH = BOAT_WIDTH_PIXEL * 3;
+    public static final int BOAT_HEIGHT = BOAT_HEIGHT_PIXEL * 3;
 }
