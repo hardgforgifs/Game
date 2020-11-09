b328cbb0627f821d5ae8db341be571f379e9afed
rmil <rhys.milling@googlemail.com>
2020-11-09 01:38:32 +0000

    Main game with boat movement

--- core/src/com/hardgforgif/dragonboatracing/screens/MainGameScreen.java
+++ core/src/com/hardgforgif/dragonboatracing/screens/MainGameScreen.java
@@ -3,6 +3,7 @@ package com.hardgforgif.dragonboatracing.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.hardgforgif.dragonboatracing.DragonBoatRacing;
 import com.hardgforgif.dragonboatracing.tools.CollisionRect;
@@ -29,6 +30,8 @@ public class MainGameScreen implements Screen {
         x = DragonBoatRacing.WIDTH / 2 - BOAT_WIDTH / 2;
         y = 15;
 
+        img = new Texture("boat1.png");
+
         playerRect = new CollisionRect(0, 0, BOAT_WIDTH, BOAT_HEIGHT);
 
         game.scrollingBackground.setSpeedFixed(false);
@@ -41,14 +44,37 @@ public class MainGameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) y += SPEED * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) y -= SPEED * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) x -= SPEED * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x += SPEED * Gdx.graphics.getDeltaTime();
+        if (isUp()) y += SPEED * Gdx.graphics.getDeltaTime();
+        if (isDown()) y -= SPEED * Gdx.graphics.getDeltaTime();
+        if (isLeft()) x -= SPEED * Gdx.graphics.getDeltaTime();
+        if (isRight()) x += SPEED * Gdx.graphics.getDeltaTime();
 
+        playerRect.move(x, y);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+
+        game.scrollingBackground.updateAndRender(delta, game.batch);
+
         game.batch.draw(img, x, y);
         game.batch.end();
+    }
+
+    private boolean isRight() {
+        return Gdx.input.isKeyPressed(Input.Keys.UP) || (Gdx.input.isTouched() && game.cam.getInputInGameWorld().x >= x);
+    }
+
+    private boolean isLeft () {
+        return Gdx.input.isKeyPressed(Input.Keys.LEFT) || (Gdx.input.isTouched() && game.cam.getInputInGameWorld().x < x);
+    }
+
+    private boolean isUp () {
+        return Gdx.input.isKeyJustPressed(Input.Keys.UP) || (Gdx.input.isTouched() && game.cam.getInputInGameWorld().y >= y);
+    }
+
+    private boolean isDown () {
+        return Gdx.input.isKeyJustPressed(Input.Keys.DOWN) || (Gdx.input.isTouched() && game.cam.getInputInGameWorld().y < y);
     }
 
     @Override
