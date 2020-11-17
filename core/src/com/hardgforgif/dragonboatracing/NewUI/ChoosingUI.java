2db414bf5b4e46198d99875e0e670b0df1483ec6
unknown <samplaneis@hotmail.com>
2020-11-17 22:29:40 +0000

    Finished up ChoosingUI

--- core/src/com/hardgforgif/dragonboatracing/NewUI/ChoosingUI.java
+++ core/src/com/hardgforgif/dragonboatracing/NewUI/ChoosingUI.java
@@ -6,135 +6,122 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
-import com.hardgforgif.dragonboatracing.entities.Boat;
 
-public class ChoosingUI {
+public class ChoosingUI extends UI{
     private Texture background;
     private Sprite background_sprite;
-    private Texture stamina;
-    private Sprite stamina_sprite;
-    private Texture robustness;
-    private Sprite robustness_sprite;
-    private Texture acceleration;
-    private Sprite acceleration_sprite;
-    private Texture handling;
-    private Sprite handling_sprite;
-    private BitmapFont stamina_label;
-    private BitmapFont robustness_label;
-    private BitmapFont acceleration_label;
-    private BitmapFont handling_label;
-    private Boat boat1;
-    private Boat boat2;
-    private Boat boat3;
-    private Boat boat4;
-    private Sprite boat1_sprite;
-    private Sprite boat2_sprite;
-    private Sprite boat3_sprite;
-    private Sprite boat4_sprite;
-    private Boat current_boat;
+    private Texture bar;
+    private Sprite bar_sprite;
+    private Texture boat;
+    private Sprite boat_sprite;
+    private BitmapFont label;
+    private Texture boat_texture;
     private final int boat1_x;
     private final int boat2_x;
     private final int boat3_x;
     private final int boat4_x;
     private final int boat_y;
+    private final int BOAT_WIDTH;
+    private final int BOAT_HEIGHT;
+    private int[] boats_stamina;
+    private int[] boats_robustness;
+    private int[] boats_acceleration;
+    private int[] boats_handling;
+    private int[] current_stats;
 
 
     public ChoosingUI() {
         background = new Texture(Gdx.files.internal("Background.png"));
         background_sprite = new Sprite(background);
         background_sprite.setPosition(250,200);
-        stamina = new Texture (Gdx.files.internal("Stamina_bar.png"));
-        robustness = new Texture (Gdx.files.internal("Robustness_bar.png"));
-        acceleration = new Texture (Gdx.files.internal("Acceleration_bar.png"));
-        handling = new Texture (Gdx.files.internal("Maneuverability_bar.png"));
-        stamina_sprite = new Sprite(stamina);
-        robustness_sprite = new Sprite(robustness);
-        acceleration_sprite = new Sprite(acceleration);
-        handling_sprite = new Sprite(handling);
-        stamina_label = new BitmapFont();
-        robustness_label = new BitmapFont();
-        acceleration_label = new BitmapFont();
-        handling_label = new BitmapFont();
-        stamina_sprite.setPosition(510,485);
-        robustness_sprite.setPosition(510,435);
-        acceleration_sprite.setPosition(510, 385);
-        handling_sprite.setPosition(510, 335);
-        //These will match the stats we end up settling on for the boats, so the player is given an
-        //accurate representation
-        boat1 = new Boat();
-        boat2 = new Boat();
-        boat3 = new Boat();
-        boat4 = new Boat();
-        //boat1_sprite = new Sprite(boat1.boatTexture);
-        //boat2_sprite = new Sprite(boat1.boatTexture);
-        //boat3_sprite = new Sprite(boat1.boatTexture);
-        //boat4_sprite = new Sprite(boat1.boatTexture);
-        boat1_sprite.scale(-0.8f);
-        boat2_sprite.scale(-0.8f);
-        boat3_sprite.scale(-0.8f);
-        boat4_sprite.scale(-0.8f);
         boat1_x = 100;
         boat2_x = 200;
         boat3_x = 300;
         boat4_x = 400;
         boat_y = 50;
-
-        //Set current_boat here so we have a boat to display initially
-        //Replace with boat1.copy() as appropriate
-        current_boat = boat1;
+        BOAT_WIDTH = 50;
+        BOAT_HEIGHT = 100;
+        boat = new Texture("Boat1.png");
     }
 
-    public void drawUI(Batch batch, float screen_width, Boat current_boat, Vector2 mousePos) {
+
+    public void drawUI(Batch batch, float screen_width, Vector2 mousePos) {
         //Checks if the mouse is hovering over a boat and updates the bars displayed as appropriate
+        //Assuming boats are 50 wide and 100 long
         if (mousePos.x > boat1_x &&
-                mousePos.x < boat1_x + boat1_sprite.getWidth() &&
+                mousePos.x < boat1_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat1_sprite.getHeight()) {
-            //Or boat1.copy() as required
-            current_boat = boat1;
-
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            current_stats[0] = boats_stamina[0];
+            current_stats[1] = boats_robustness[0];
+            current_stats[2] = boats_acceleration[0];
+            current_stats[3] = boats_handling[0];
         } else if (mousePos.x > boat2_x &&
-                mousePos.x < boat2_x + boat2_sprite.getWidth() &&
+                mousePos.x < boat2_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat2_sprite.getHeight()) {
-            //Or boat2.copy() as required
-            current_boat = boat2;
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            current_stats[0] = boats_stamina[1];
+            current_stats[1] = boats_robustness[1];
+            current_stats[2] = boats_acceleration[1];
+            current_stats[3] = boats_handling[1];
         } else if (mousePos.x > boat3_x &&
-                mousePos.x < boat3_x + boat3_sprite.getWidth() &&
+                mousePos.x < boat3_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat3_sprite.getHeight()) {
-            //Or boat3.copy() as required
-            current_boat = boat3;
-
+                mousePos.y < boat_y + + BOAT_HEIGHT) {
+            current_stats[0] = boats_stamina[2];
+            current_stats[1] = boats_robustness[2];
+            current_stats[2] = boats_acceleration[2];
+            current_stats[3] = boats_handling[2];
         } else if (mousePos.x > boat4_x &&
-                mousePos.x < boat4_x + boat4_sprite.getWidth() &&
+                mousePos.x < boat4_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat4_sprite.getHeight()) {
-            //Or boat4.copy() as required
-            current_boat = boat4;
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            current_stats[0] = boats_stamina[3];
+            current_stats[1] = boats_robustness[3];
+            current_stats[2] = boats_acceleration[3];
+            current_stats[3] = boats_handling[3];
         }
-        float full_bar = screen_width / 3;
-        // Lines below may need some work, I don't know what the max values are
-        //Or what the getter methods will be named
-        stamina_sprite.setSize(full_bar * (current_boat.stamina/max_stamina), 30);
-        robustness_sprite.setSize(full_bar * (current_boat.robustness/max_robustness), 30);
-        acceleration_sprite.setSize(full_bar * (current_boat.acceleration/max_acceleration), 30);
-        handling_sprite.setSize(full_bar * (current_boat.handling/max_handling), 30);
-        //Draws components to the screen
-        batch.begin();
-        stamina_label.draw(batch, "Stamina:", 450, 500);
-        robustness_label.draw(batch, "Robustness:", 450, 450);
-        acceleration_label.draw(batch, "Acceleration:", 450, 400);
-        handling_label.draw(batch, "Handling:", 450, 350);
-        stamina_sprite.draw(batch);
-        robustness_sprite.draw(batch);
-        acceleration_sprite.draw(batch);
-        handling_sprite.draw(batch);
 
-        boat1_sprite.draw(batch);
-        boat2_sprite.draw(batch);
-        boat3_sprite.draw(batch);
-        boat4_sprite.draw(batch);
+        float full_bar = screen_width / 3;
+        batch.begin();
+
+        bar_sprite.setSize(full_bar * (current_stats[0]/200), 30);
+        bar_sprite.setPosition(510,485);
+        bar = new Texture("Stamina_bar.png");
+        bar_sprite.setTexture(bar);
+        bar_sprite.draw(batch);
+
+        bar_sprite.setSize(full_bar * (current_stats[1]/200), 30);
+        bar_sprite.setPosition(510,435);
+        bar = new Texture("Robustness_bar.png");
+        bar_sprite.setTexture(bar);
+        bar_sprite.draw(batch);
+
+        bar_sprite.setSize(full_bar * (current_stats[2]/200), 30);
+        bar_sprite.setPosition(510,385);
+        bar = new Texture("Acceleration_bar.png");
+        bar_sprite.setTexture(bar);
+        bar_sprite.draw(batch);
+
+        bar_sprite.setSize(full_bar * (current_stats[3]/200), 30);
+        bar_sprite.setPosition(510,335);
+        bar = new Texture("Maneuverability_bar.png");
+        bar_sprite.setTexture(bar);
+        bar_sprite.draw(batch);
+
+        label.draw(batch, "Stamina:", 450, 500);
+        label.draw(batch, "Robustness:", 450, 450);
+        label.draw(batch, "Acceleration:", 450, 400);
+        label.draw(batch, "Handling:", 450, 350);
+
+        boat_sprite.setPosition(boat1_x, boat_y);
+        boat_sprite.draw(batch);
+        boat_sprite.setPosition(boat2_x, boat_y);
+        boat_sprite.draw(batch);
+        boat_sprite.setPosition(boat3_x, boat_y);
+        boat_sprite.draw(batch);
+        boat_sprite.setPosition(boat4_x, boat_y);
+        boat_sprite.draw(batch);
 
         batch.end();
 
@@ -148,46 +135,48 @@ public class ChoosingUI {
     public void getInput(Vector2 mousePos) {
         //Checks if the mouse is hovering over a boat and updates the bars displayed as appropriate
         if (mousePos.x > boat1_x &&
-                mousePos.x < boat1_x + boat1_sprite.getWidth() &&
+                mousePos.x < boat1_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat1_sprite.getHeight()) {
-            //Or boat1.copy() as required
-            //GameData.playerBoat = boat1;
+                mousePos.y < boat_y +BOAT_HEIGHT) {
+            GameData.stamina = boats_stamina[0];
+            GameData.robustness = boats_robustness[0];
+            GameData.acceleration = boats_acceleration[0];
+            GameData.handling = boats_handling[0];
 
         } else if (mousePos.x > boat2_x &&
-                mousePos.x < boat2_x + boat2_sprite.getWidth() &&
+                mousePos.x < boat2_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat2_sprite.getHeight()) {
-            //Or boat2.copy() as required
-            //current_boat = boat2;
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            GameData.stamina = boats_stamina[1];
+            GameData.robustness = boats_robustness[1];
+            GameData.acceleration = boats_acceleration[1];
+            GameData.handling = boats_handling[1];
         } else if (mousePos.x > boat3_x &&
-                mousePos.x < boat3_x + boat3_sprite.getWidth() &&
+                mousePos.x < boat3_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat3_sprite.getHeight()) {
-            //Or boat3.copy() as required
-            //current_boat = boat3;
-
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            GameData.stamina = boats_stamina[2];
+            GameData.robustness = boats_robustness[2];
+            GameData.acceleration = boats_acceleration[2];
+            GameData.handling = boats_handling[2];
         } else if (mousePos.x > boat4_x &&
-                mousePos.x < boat4_x + boat4_sprite.getWidth() &&
+                mousePos.x < boat4_x + BOAT_WIDTH &&
                 mousePos.y > boat_y &&
-                mousePos.y < boat_y + boat4_sprite.getHeight()) {
-            //Or boat4.copy() as required
-            //current_boat = boat4;
+                mousePos.y < boat_y + BOAT_HEIGHT) {
+            GameData.stamina = boats_stamina[3];
+            GameData.robustness = boats_robustness[3];
+            GameData.acceleration = boats_acceleration[3];
+            GameData.handling = boats_handling[3];
         }
 
     }
 
 
     public void dispose() {
-        stamina_label.dispose();
-        robustness_label.dispose();
-        acceleration_label.dispose();
-        handling_label.dispose();
+        label.dispose();
+        boat.dispose();
         background.dispose();
-        stamina.dispose();
-        robustness.dispose();
-        acceleration.dispose();
-        handling.dispose();
-
+        bar.dispose();
+        background.dispose();
     }
 }
