0c94bfd7c285598200a82b5c310617a0e3af355c
Dragos Stoican <dragos.stoican28@gmail.com>
2020-11-23 22:38:21 +0000

    Added first release

---
+++ desktop/out/production/resources/Map1/red2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="solid-red-09" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="solid-red-09.png" width="1920" height="1080"/>
+</tileset>
